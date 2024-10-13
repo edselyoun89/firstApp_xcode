32d0c1ba6f2e8fc82306cb785cedb8c3488d99ee
edselyoun89 <nikita.telichko.gmail.com@yandex.ru>
2024-10-13 14:05:06 +0300

    first Xcode

---
+++ first/firstApp.swift
@@ -0,0 +1,19 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by user on 13.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+//zxc
+
